3a308de2c49f45e185d02cb997351441e08437bc
Ozan YILDIZ <53907221+ozanyldzgithuboffical@users.noreply.github.com>
2020-03-16 11:11:41 +0300

    Update GlobalRequestMappingWithoutMethodLevel.java

--- GlobalRequestMappingWithoutMethodLevel.java
+++ GlobalRequestMappingWithoutMethodLevel.java
@@ -6,7 +6,7 @@ EmployeeDAOManager manager;
 //<BASE_URL> - it will be accessed via same url
 @RequestMapping(method=RequestMethod.GET)
 public String getAllEmployees(){
-
+  Model model= new Model();
   List<Employee> employeeList=manager.getAllEmployees();
   model.addAttribute("employees",employeeList);
   return "employeelist";
