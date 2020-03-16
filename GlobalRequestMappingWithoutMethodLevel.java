6286c571d0e51339b5ad1a08bd494fece1a53b6c
Ozan YILDIZ <53907221+ozanyldzgithuboffical@users.noreply.github.com>
2020-03-16 11:08:38 +0300

    Create Global Request Mapping only with method
    Create Global Request Mapping only with method

---
+++ GlobalRequestMappingWithoutMethodLevel.java
@@ -0,0 +1,15 @@
+@Controller
+@RequestMapping("/employee-business")
+public class EmployeeController{
+@Autowired
+EmployeeDAOManager manager;
+//<BASE_URL> - it will be accessed via same url
+@RequestMapping(method=RequestMethod.GET)
+public String getAllEmployees(){
+
+  List<Employee> employeeList=manager.getAllEmployees();
+  model.addAttribute("employees",employeeList);
+  return "employeelist";
+
+}
+}
